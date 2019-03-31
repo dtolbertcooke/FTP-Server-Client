c18feb314790c0cce4574f05db06838d05286367
Doug <dtolbertcooke@gmail.com>
2019-03-31 17:13:45 -0400

    debugging

--- ftp-server.py
+++ ftp-server.py
@@ -1,12 +1,13 @@
 from pyftpdlib.authorizers import DummyAuthorizer
-from pyftpdlib.authorizers import FTPHandler
-from pyftpdlib.authorizers import FTPServer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
 
 authorizer = DummyAuthorizer()
-authorizer.add_user('user', '12345', '/home/dtolbertcooke', perm='elradfmw')
-authorizer.add_anonymous('/home/dtolbertcooke', perm='elradfmw')
+authorizer.add_user("user", "12345", "/home/dtolbertcooke", perm="elradfmw")
+authorizer.add_anonymous("/home/dtolbertcooke", perm="elradfmw")
 
 handler = FTPHandler
-handler.authorizer = authorizers
-server = FTPServer(('127.0.0.1', 1026), handler)
+handler.authorizer = authorizer
+
+server = FTPServer(("127.0.0.1", 1026), handler)
 server.serve_forever()
